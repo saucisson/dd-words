4046e0d1103b0dae12e42b1f3c936c00afe498dd
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-08-13 15:10:54 +0200

    Use libsdd nary sum.

--- src/sdd-fixed.cc
+++ src/sdd-fixed.cc
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -5,6 +6,7 @@
 #include <vector>
 
 #include "sdd/sdd.hh"
+#include "sdd/tools/size.hh"
 
 using namespace std;
 
@@ -17,53 +19,100 @@ struct conf
 using SDD         = sdd::SDD<conf>;
 using values_type = conf::Values;
 
-int main (int argc, const char** argv)
+int
+main (int argc, const char** argv)
 {
-  auto manager = sdd::init<conf>();
-  vector<SDD> collections;
+  const auto subsize = 10000;
 
-  size_t max_size = stoi (argv [1]);
+  vector<SDD> collections;
+  vector<SDD> subcollection;
+  subcollection.reserve(subsize);
+
+  conf c;
+  c.final_cleanup = false; // don't cleanup memory on manager scope exit.
+  c.hom_cache_size = 2; // we don't use homomorphisms.
+  c.hom_unique_table_size = 2; // we don't use homomorphisms.
+  auto manager = sdd::init<conf>(c);
+
+  if (argc == 0)
+  {
+    cerr << "No arguments" << endl;
+    return 1;
+  }
+
+  string line;
+  line.reserve(256);
+
+  const size_t max_size = [&]
+  {
+    size_t max = 0;
+    for (size_t param = 1; param < argc; ++param)
+    {
+      const string filename = argv[param];
+      ifstream dict(filename);
+      if (dict.is_open())
+      {
+        while (std::getline(dict, line))
+        {
+          max = max > line.size() ? max : line.size();
+        }
+      }
+      else
+      {
+        cerr << "Warning, can't open " << filename << endl;
+      }
+    }
+    return max;
+  }();
+
+  cout << "Max word length is " << max_size << endl;
+
   // Construct the SDD order: we need one level per letter.
   vector<unsigned int> v(max_size);
   iota(v.begin(), v.end(), 0);
   sdd::order_builder<conf> ob;
   const sdd::order<conf> order(sdd::order_builder<conf>(v.begin(), v.end()));
 
-  string line;
-  for (size_t param = 2; param < argc; ++param)
+  for (size_t param = 1; param < argc; ++param)
   {
-    cout << argv [param] << endl;
+    cout << argv[param] << endl;
     string filename = argv[param];
     ifstream dict (filename);
     if (dict.is_open())
     {
       size_t count = 0;
-      SDD subcollection = sdd::zero<conf>();
+
       while (getline(dict, line))
       {
         count++;
-        subcollection += SDD(order, [&](unsigned int pos) {
-          return pos < line.size()
-               ? values_type {line[pos]}
-               : values_type {'#'};
-        });
-        if (count == 10000)
+        subcollection.emplace_back(SDD(order, [&](unsigned int pos) 
+                                              {
+                                                return pos < line.size()
+                                                     ? values_type {line[pos]}
+                                                     : values_type {'#'};
+                                              }));
+        if (count == subsize)
         {
-          collections.push_back (subcollection);
-          subcollection = sdd::zero<conf>();
+          collections.emplace_back(sdd::sum<conf>(subcollection.cbegin(), subcollection.cend()));
+          subcollection.clear();
           count = 0;
           cout << "." << flush;
         }
       }
-      collections.push_back (subcollection);
+      collections.emplace_back(sdd::sum<conf>(subcollection.cbegin(), subcollection.cend()));
+    }
+    else
+    {
+      cerr << "Warning, can't open " << filename << endl;
     }
     dict.close();
     cout << endl;
   }
   cout << endl;
-  SDD collection = sdd::zero<conf>();
-  for (const SDD& d : collections)
-    collection += d;
+  const auto collection = sdd::sum<conf>(collections.cbegin(), collections.cend());
   cout << endl;
   cout << "# Words: " << collection.size() << endl;
+  cout << "size: " << sdd::tools::size(collection) << " bytes" << endl;
+
+  return 0;
 }
