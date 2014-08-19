5757bae68f085633ce80bfd54379786a963e42b6
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-08-19 22:44:00 +0200

    Canonize data.

--- src/order/generate.lua
+++ src/order/generate.lua
@@ -7,13 +7,20 @@ if #arg ~= 1 then
   print (tostring (arg [0]) .. " <json file>")
   os.exit (1)
 end
+local name = string.gsub (arg [1], ".json", "")
 
 local json = require "dkjson"
 
-local file = io.open (arg [1], "r")
-local text = file:read ("*all")
-local data = json.decode (text)
-file:close ()
+local key_list = {}
+local key_set = {}
+
+local data = (function ()
+  local file = io.open (name .. ".json", "r")
+  local text = file:read ("*all")
+  local data = json.decode (text)
+  file:close ()
+  return data
+end) ()
 
 local function is_value (x)
   return type (x) ~= "table"
@@ -52,8 +59,61 @@ local function order (x, prefix)
     for i = 1, list_size do
       result [#result + 1] = order (x [1], prefix .. "/" .. tostring (i))
     end
+    for i = #x, list_size do
+      x [i] = json.null
+    end
     return result
   end
 end
 
-print (json.encode (order (data [1])))
+local function canonize (x)
+  if is_value (x) then
+    return prefix
+  elseif is_map (x) then
+    for k, v in pairs (x) do
+      key_set [k] = true
+      canonize (v)
+    end
+  elseif is_list (x) then
+    for i, v in ipairs (x) do
+      canonize (v)
+    end
+    for i = #x + 1, list_size do
+      x [i] = json.null
+    end
+  end
+end
+
+local state = {
+  indent   = true,
+  level    = 0,
+  keyorder = key_list,
+}
+
+canonize (data)
+
+do
+  local result = order (data [1])
+  for k in pairs (key_set) do
+    key_list [#key_list + 1] = k
+  end
+  table.sort (key_list)
+  local file = io.open (name .. ".order.json", "w")
+  local text = json.encode (result, state)
+  file:write (text)
+  file:close ()
+  print ("Generated order in " .. name .. ".order.json.")
+end
+
+do
+  for i, v in ipairs (data) do
+    if v == json.null then
+      data [i] = nil
+    end
+  end
+  local file = io.open (name .. ".json", "w")
+  local text = json.encode (data, state)
+  file:write (text)
+  file:close ()
+  print ("Canonized data in " .. name .. ".json.")
+end
